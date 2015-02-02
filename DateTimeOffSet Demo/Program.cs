2970c51f422ffb113e9140048ed588bcf9ca8245
codemasta <seun2talk@gmail.com>
2015-02-02 18:06:56 +0100

    Initial Code For Local Time in DateTime.Now

---
+++ DateTimeOffSet Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateTimeOffSet_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentTime = DateTime.Now;
+            var nigerianTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(currentTime, "W. Central Africa Standard Time");
+            Console.WriteLine(nigerianTime.ToLongTimeString());
+            Console.ReadLine();
+        }
+    }
+}
